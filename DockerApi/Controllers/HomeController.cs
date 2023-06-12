b640e87b2f62eddd94e58b60a81ee4700be53c41
Pradee67 <109140915+Pradee67@users.noreply.github.com>
2023-06-12 11:44:15 +0530

    added code

---
+++ DockerApi/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DockerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string GetName()
+        {
+            string name = "Hello";
+            return name;
+        }
+    }
+}
