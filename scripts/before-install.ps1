2e92e6c0fb49e8135612bb77ee873e6b95ea65fd
Pradee67 <109140915+Pradee67@users.noreply.github.com>
2023-06-28 15:52:52 +0530

    changed scripts

--- scripts/before-install.ps1
+++ scripts/before-install.ps1
@@ -1,21 +1,2 @@
-# Stop the existing API service (if running)
-Stop-Service -Name "PipelineTesting" -ErrorAction SilentlyContinue
-
-# Backup existing API files (optional)
-$backupPath = "C:\Backup\"
-if (!(Test-Path $backupPath)) {
-    New-Item -ItemType Directory -Path $backupPath | Out-Null
-}
-Copy-Item -Path "C:\inetpub\wwwroot" -Destination "$backupPath" -Recurse -Force
-
-# Clear the destination directory
-Remove-Item -Path "C:\inetpub\wwwroot\*" -Recurse -Force
-
-# Install any required dependencies (e.g., .NET Framework, runtime components)
-
-# Set up environment variables (if necessary)
-
-# Additional setup tasks or configurations specific to your application
-
-# Start any required services (if necessary)
-Start-Service -Name "PipelineTesting"
+Import-Module -Name ServerManager
+Install-WindowsFeature Web-Server
